97a9f46589e0c4752cf63f16bbe30b0c42e06912
Emmanuelitgit <eyidana001@st.ug.edu.gh>
2024-08-13 15:39:56 +0000

    db

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
   waitForConnections: true,
-  connectionLimit: 20,
+  connectionLimit: 1500,
   queueLimit: 0
 });
 
